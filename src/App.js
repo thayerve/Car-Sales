0d297793e54fb6f143b26d7df740d7423bec8126
Virginia Thayer <virginia@stripe.com>
2019-08-14 15:30:01 -0700

    set up initial state and reducer... actions WIP

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    store: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+const App = (props) => {
+  // took out given state const and moved to initialState in rootReducer
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
@@ -34,15 +19,30 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        {/* changed all state.thing calls to props.thing */}
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={props.store} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+// added mapStateToProps
+const mapStateToProps = state => {
+  console.log('state: ', state);
+  return {
+    car: state.car,
+    store: state.store,
+    additionalPrice: state.additionalPrice
+  };
+};
+
+// exported with connect
+export default connect(
+  mapStateToProps,
+  // { removeFeature, buyItem }
+)(App);
